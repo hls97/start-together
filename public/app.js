bbfc8b1001a10ebefede98f6b695767deaa66607
Harold Shen <hlshen@ucsd.edu>
2017-04-05 19:50:51 -0700

    initial

---
+++ public/app.js
@@ -0,0 +1,59 @@
+angular
+.module('start', ['btford.socket-io'
+])
+.factory('mySocket', function (socketFactory) {
+     var mySocket = socketFactory();
+  return mySocket;
+})
+.controller('main', function($scope, $interval, mySocket){
+    $scope.seconds = 30;
+    $scope.stopTime;
+    $scope.toggleTime = 1;
+    $scope.totalTime = 60;
+    $scope.percent = 0.0;
+
+    mySocket.on('start', function () {
+        $scope.startTimer()
+    });
+    mySocket.on('stop', function () {
+        $scope.stopTimer()
+    });
+    mySocket.on('change', function (val) {
+        $scope.seconds = val
+    });
+
+    $scope.emitStart = function(){
+        mySocket.emit('start',{});
+        $scope.startTimer()
+    }
+    $scope.emitStop = function(){
+        console.log("stop")
+        mySocket.emit('stop',{});
+        $scope.stopTimer()
+    }
+    $scope.emitChange = function(){
+        console.log("CHANGING")
+        mySocket.emit('change',{val:$scope.seconds});
+    }
+    $scope.startTimer = function(){
+        if($scope.stopTime !== undefined){
+            $interval.cancel($scope.stopTime);
+        }
+
+        $scope.stopTime = $interval($scope.decSeconds, 1000);
+    }
+
+    $scope.decSeconds = function(){
+        $scope.seconds--;
+        $scope.percent = ($scope.totalTime-$scope.seconds)/$scope.totalTime;
+        if($scope.seconds == 0){
+            $interval.cancel($scope.stopTime);
+            $scope.openToast();
+        }
+    }
+
+    $scope.stopTimer = function(){
+        $interval.cancel($scope.stopTime);
+
+    }
+});
---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var http = require('http').Server(app);
+var port = process.env.PORT || 8080;
+var io = require('socket.io')(http);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/public'));
+//app.use('/', express.static(__dirname));
+//var routes = require("./routes/routes");
+
+
+
+http.listen(8080, "127.0.0.1");
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('start',function(){
+      console.log("start")
+      io.emit('start');
+  })
+  socket.on('stop',function(){
+            console.log("stop")
+      io.emit('stop');
+  })
+  socket.on('change',function(val){
+
+    var num =val.val
+        console.log(num)
+      io.emit('change', num);
+  })
+});
+
+
+module.exports = app;
