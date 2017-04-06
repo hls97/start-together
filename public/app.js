d27178aef2e110b5d8456e2b9d66d4844998dcc7
Harold Shen <hlshen@ucsd.edu>
2017-04-06 00:22:19 -0700

    setup gulp and angular-ui-router

--- public/app.js
+++ public/app.js
@@ -1,59 +1,29 @@
 angular
-.module('start', ['btford.socket-io'
+.module('start', ['btford.socket-io',
+                'ui.router'
 ])
 .factory('mySocket', function (socketFactory) {
      var mySocket = socketFactory();
   return mySocket;
 })
-.controller('main', function($scope, $interval, mySocket){
-    $scope.seconds = 30;
-    $scope.stopTime;
-    $scope.toggleTime = 1;
-    $scope.totalTime = 60;
-    $scope.percent = 0.0;
+.config(function($stateProvider, $urlRouterProvider) {
 
-    mySocket.on('start', function () {
-        $scope.startTimer()
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+    // .state('login',{
+    //     url: '/',
+    //     templateUrl: 'views/login.html',
+    //     controller: 'LoginCtrl',
+    //     params:{
+    //         from: null,
+    //         who: null
+    //     }
+    // })
+    .state('lobby', {
+        url:'/lobby',
+        templateUrl: 'pages/lobby/lobby.html',
+        controller: 'lobbyCtrl'
     });
-    mySocket.on('stop', function () {
-        $scope.stopTimer()
-    });
-    mySocket.on('change', function (val) {
-        $scope.seconds = val
-    });
 
-    $scope.emitStart = function(){
-        mySocket.emit('start',{});
-        $scope.startTimer()
-    }
-    $scope.emitStop = function(){
-        console.log("stop")
-        mySocket.emit('stop',{});
-        $scope.stopTimer()
-    }
-    $scope.emitChange = function(){
-        console.log("CHANGING")
-        mySocket.emit('change',{val:$scope.seconds});
-    }
-    $scope.startTimer = function(){
-        if($scope.stopTime !== undefined){
-            $interval.cancel($scope.stopTime);
-        }
 
-        $scope.stopTime = $interval($scope.decSeconds, 1000);
-    }
-
-    $scope.decSeconds = function(){
-        $scope.seconds--;
-        $scope.percent = ($scope.totalTime-$scope.seconds)/$scope.totalTime;
-        if($scope.seconds == 0){
-            $interval.cancel($scope.stopTime);
-            $scope.openToast();
-        }
-    }
-
-    $scope.stopTimer = function(){
-        $interval.cancel($scope.stopTime);
-
-    }
-});
+})
